fafc23fdaa9c3aa4c8bfb8f0c372fcfc2df28ebe
Geir Arne Hjelle <geirarne@gmail.com>
2019-10-22 18:21:36 +0200

    Use toml package instead of pytoml

--- pyconfs/readers/toml.py
+++ pyconfs/readers/toml.py
@@ -1,6 +1,5 @@
 """Reader for TOML-files
 
-
 """
 
 # Standard library imports
@@ -9,16 +8,21 @@ from typing import Any, Dict
 
 # Third party imports
 import pyplugs
-import pytoml
+
+# PyConfs imports
+from pyconfs._util import delayed_import
+
+# Delayed imports
+toml = delayed_import("toml")
 
 _SUFFIXES = {".toml"}
 
 
 @pyplugs.register
 def read_toml_file(file_path: pathlib.Path) -> Dict[str, Any]:
-    """Use pytoml library to read TOML file"""
+    """Use toml library to read TOML file"""
     with file_path.open(mode="r") as fid:
-        cfg = pytoml.load(fid)
+        cfg = toml.load(fid)
 
     return cfg
 
