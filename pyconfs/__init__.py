e1c3417a523b5b179808db009e019f94123b008a
Geir Arne Hjelle <geirarne@gmail.com>
2020-02-19 15:22:28 +0100

    Bump version: 0.3.1 → 0.3.2

--- pyconfs/__init__.py
+++ pyconfs/__init__.py
@@ -18,7 +18,7 @@ from pyconfs.configuration import Configuration  # noqa
 # Version of PyConfs
 #
 # This is automatically set using the bumpversion tool
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 
 # Homepage for PyConfs
