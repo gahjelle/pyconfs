683587fa58f172bdf073dca2ce9048adca24f6d8
Geir Arne Hjelle <geirarne@gmail.com>
2020-11-28 19:10:33 +0100

    Add basic tests that config files can be opened

---
+++ tests/test_files.py
@@ -0,0 +1,31 @@
+"""Test reading of configuration files"""
+
+from pyconfs import Configuration
+import pathlib
+import pytest
+
+
+@pytest.fixture
+def sample_path():
+    """Path to sample files"""
+    return pathlib.Path(__file__).parent / "files"
+
+
+def test_read_ini(sample_path):
+    """Test that reading an INI file succeeds"""
+    Configuration.from_file(sample_path / "sample.ini")
+
+
+def test_read_json(sample_path):
+    """Test that reading an JSON file succeeds"""
+    Configuration.from_file(sample_path / "sample.json")
+
+
+def test_read_toml(sample_path):
+    """Test that reading an TOML file succeeds"""
+    Configuration.from_file(sample_path / "sample.toml")
+
+
+def test_read_yaml(sample_path):
+    """Test that reading an YAML file succeeds"""
+    Configuration.from_file(sample_path / "sample.yaml")
