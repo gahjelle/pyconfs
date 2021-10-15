3ac376f2d99bc73391ffe19c9d867f7704ff7344
Lewi Uberg <43423020+lewiuberg@users.noreply.github.com>
2021-10-15 09:44:21 +0200

    Enforce string keys when writing TOML config (#17)
    - Add tests
    - Enforce string keys in TOML writer
    
    Co-authored-by: Geir Arne Hjelle <geirarne@gmail.com>

--- pyconfs/writers/toml.py
+++ pyconfs/writers/toml.py
@@ -21,7 +21,20 @@ def as_toml(
     config: Dict[str, Any], pretty_print: bool = False, **toml_args: Any
 ) -> None:
     """Use toml library to write TOML file"""
+    config_with_str_keys = _enforce_str_keys(config)
+
     if pretty_print:
-        return Configuration.from_dict(config).as_str(**toml_args)
+        return Configuration.from_dict(config_with_str_keys).as_str(
+            **toml_args
+        )
     else:
-        return toml.dumps(config, **toml_args)
+        return toml.dumps(config_with_str_keys, **toml_args)
+
+
+def _enforce_str_keys(data: Dict[str, Any]) -> Dict[str, Any]:
+    """Changes keys in nested dicts to strings."""
+    if isinstance(data, dict):
+        return {
+            str(key): _enforce_str_keys(value) for key, value in data.items()
+        }
+    return data
--- tests/test_files.py
+++ tests/test_files.py
@@ -16,6 +16,22 @@ def sample_dir():
     return pathlib.Path(__file__).parent / "files"
 
 
+@pytest.fixture
+def cfg_with_int_key():
+    """Configuration with a key that is an integer"""
+    return Configuration.from_dict(
+        {
+            "answer": {42: "Life, the Universe, and Everything"},
+            "long_answer": {
+                "parts": {
+                    1: "The Answer to the Ultimate Question of",
+                    2: "Life, the Universe, and Everything",
+                },
+            },
+        }
+    )
+
+
 def test_read_ini(sample_dir):
     """Test that reading an INI file succeeds"""
     Configuration.from_file(sample_dir / "sample.ini")
@@ -27,6 +43,26 @@ def test_read_ini_sources(sample_dir):
     cfg = Configuration.from_file(cfg_path)
 
     assert cfg.sources == {f"{cfg_path} (INI reader)"}
+
+
+@pytest.mark.skip("INI does not yet support advanced reading")
+def test_ini_roundtrip(cfg):
+    """Test that a configuration can be recovered after being stored as INI"""
+    roundtripped = Configuration.from_str(
+        cfg.as_str(format="ini"), format="ini"
+    )
+
+    assert roundtripped.as_dict() == cfg.as_dict()
+
+
+def test_ini_handle_int_key_gracefully(cfg_with_int_key):
+    """Test that INI format handles int keys gracefully"""
+    roundtripped = Configuration.from_str(
+        cfg_with_int_key.as_str(format="ini"), format="ini"
+    )
+
+    # INI converts integer keys to strings
+    assert roundtripped.answer.entry_keys == ["42"]
 
 
 def test_read_json(sample_dir):
@@ -42,6 +78,25 @@ def test_read_json_sources(sample_dir):
     assert cfg.sources == {f"{cfg_path} (JSON reader)"}
 
 
+def test_json_roundtrip(cfg):
+    """Test that a configuration can be recovered after being stored as JSON"""
+    roundtripped = Configuration.from_str(
+        cfg.as_str(format="json"), format="json"
+    )
+
+    assert roundtripped.as_dict() == cfg.as_dict()
+
+
+def test_json_handle_int_key_gracefully(cfg_with_int_key):
+    """Test that JSON format handles int keys gracefully"""
+    roundtripped = Configuration.from_str(
+        cfg_with_int_key.as_str(format="json"), format="json"
+    )
+
+    # JSON converts integer keys to strings
+    assert roundtripped.answer.entry_keys == ["42"]
+
+
 def test_read_toml(sample_dir):
     """Test that reading a TOML file succeeds"""
     Configuration.from_file(sample_dir / "sample.toml")
@@ -55,6 +110,26 @@ def test_read_toml_sources(sample_dir):
     assert cfg.sources == {f"{cfg_path} (TOML reader)"}
 
 
+def test_toml_roundtrip(cfg):
+    """Test that a configuration can be recovered after being stored as TOML"""
+    roundtripped = Configuration.from_str(
+        cfg.as_str(format="toml"), format="toml"
+    )
+
+    assert roundtripped.as_dict() == cfg.as_dict()
+
+
+def test_toml_handle_int_key_gracefully(cfg_with_int_key):
+    """Test that TOML format handles int keys gracefully"""
+    roundtripped = Configuration.from_str(
+        cfg_with_int_key.as_str(format="toml"), format="toml"
+    )
+
+    # TOML converts integer keys to strings
+    assert roundtripped.answer.entry_keys == ["42"]
+    assert roundtripped.long_answer.parts.entry_keys == ["1", "2"]
+
+
 def test_read_yaml(sample_dir):
     """Test that reading a YAML file succeeds"""
     Configuration.from_file(sample_dir / "sample.yaml")
@@ -66,3 +141,22 @@ def test_read_yaml_sources(sample_dir):
     cfg = Configuration.from_file(cfg_path)
 
     assert cfg.sources == {f"{cfg_path} (YAML reader)"}
+
+
+def test_yaml_roundtrip(cfg):
+    """Test that a configuration can be recovered after being stored as YAML"""
+    roundtripped = Configuration.from_str(
+        cfg.as_str(format="yaml"), format="yaml"
+    )
+
+    assert roundtripped.as_dict() == cfg.as_dict()
+
+
+def test_yaml_handle_int_key_gracefully(cfg_with_int_key):
+    """Test that YAML format handles int keys gracefully"""
+    roundtripped = Configuration.from_str(
+        cfg_with_int_key.as_str(format="yaml"), format="yaml"
+    )
+
+    # YAML supports integer keys
+    assert roundtripped.as_dict() == cfg_with_int_key.as_dict()
